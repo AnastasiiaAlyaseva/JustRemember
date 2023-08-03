3974bdceac190787cf7b1985857c30eb521932e0
Anastasiia Alyaseva <180915an@gmail.com>
2023-08-03 15:33:43 +0200

    added repeating notification logic

--- JustRemember/Models/NotificationService.swift
+++ JustRemember/Models/NotificationService.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 import UserNotifications
 
-class Notifications {
+class NotificationService {
     func requestPermission() {
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
             if success {
@@ -21,6 +21,17 @@ class Notifications {
         let triggerDate = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: date)
         let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDate, repeats: false)
         
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request)
+    }
+    func sendRepeatingNotification(title: String, subtitle: String, reapeatInterval: TimeInterval) {
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.subtitle = subtitle
+        content.sound = UNNotificationSound.default
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: reapeatInterval, repeats: true)
         
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
         UNUserNotificationCenter.current().add(request)
--- JustRemember/Views/SettingsView.swift
+++ JustRemember/Views/SettingsView.swift
@@ -4,14 +4,13 @@ struct SettingsView: View {
     @StateObject private var storage = Storage()
     @State private var toggleIsActive = false
     @State private var selectedDate = Date()
-    let notify = Notifications()
+    @State private var repeatInterval = 5
+    let notify = NotificationService()
     
     var body: some View {
         NavigationStack {
-            
             Form{
-                
-                    Image("profileIcon")
+                Image("profileIcon")
                     .resizable()
                     .frame(width: 100, height: 100)
                     .clipShape(Circle())
@@ -29,12 +28,27 @@ struct SettingsView: View {
                         Button("Schedule notification") {
                             notify.sendNotification(at: selectedDate, title: title, subtitle: subtitle)
                         }
+                        Picker("Reapeat Interval", selection: $repeatInterval) {
+                            Text("5 minutes").tag(5)
+                            Text("10 minutes").tag(10)
+                            Text("15 minutes").tag(15)
+                            Text("20 minutes").tag(20)
+                        }
+                        .pickerStyle(MenuPickerStyle())
+                        
+                        Button("Reapeat notification") {
+                            let interval = TimeInterval(repeatInterval * 60)
+                            let title = storage.getCollections()[1].words[1].word
+                            let subtitle = storage.getCollections()[1].words[1].meaning
+                            
+                            notify.sendRepeatingNotification(title: title, subtitle: subtitle, reapeatInterval: interval)
+                        }
                     }
                 }
             }
             .navigationTitle("Settings")
         }
-        }
+    }
 }
 
 struct SettingsView_Previews: PreviewProvider {
