1ee2587c123064cfde87e13dd5af2e6a2ac90443
Anastasiia Alyaseva <180915an@gmail.com>
2023-08-08 16:43:10 +0200

    added version display

---
+++ JustRemember/Services/AppVersionProvider.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+enum AppVersionProvider {
+    static func appVersion(in bundle: Bundle = .main) -> String {
+        guard let version = bundle.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String else {
+            fatalError("CFBundleShortVersionString should not be missing from info dictionary")
+        }
+        return version
+    }
+    static func appBuild(in bundle: Bundle = .main) -> String {
+        guard let build = bundle.object(forInfoDictionaryKey: "CFBundleVersion") as? String else {
+            fatalError("CFBundleVersion should not be missing from info dictionary")
+        }
+        return build
+    }
+}
+
